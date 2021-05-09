a185cb0b97003194c2db6be31f6b55e1bdee275a
WBowling123 <59381426+WBowling123@users.noreply.github.com>
2021-05-08 23:12:53 -0400

    add model item

--- items/models.py
+++ items/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=180)
+    type = models.CharField(max_length=120)  # TODO: ForeignKey
+    category = models.CharField(max_length=120)  # TODO: ForeignKey
+    size = models.CharField(max_length=40)
+
+    class Meta:
+        verbose_name = "Item Parent Model"
+        db_table = 'item_parent'
