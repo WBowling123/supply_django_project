c391c0839e2ba8c59287c424d430b32c3468a830
WBowling123 <59381426+WBowling123@users.noreply.github.com>
2021-05-09 00:15:51 -0400

    migrate new models created in models.py

--- items/models.py
+++ items/models.py
@@ -1,12 +1,52 @@
+import datetime
+
 from django.db import models
+
+from django.contrib.auth.models import User
+
+
+class ItemCategory(models.Model):
+    name = models.CharField(max_length=120)
+
+    class Meta:
+        db_table = 'item_category'
+
+    def __str__(self):
+        return self.name
+
+
+class ItemType(models.Model):
+    name = models.CharField(max_length=180)
+    ITEM_TYPE_TAG_CHOICES = [
+        ('M', 'MEDICATION'),
+        ('S', 'SUPPLY')
+    ]
+    item_type_tag = models.CharField(choices=ITEM_TYPE_TAG_CHOICES, max_length=100)
+
+    class Meta:
+        db_table = 'item_type'
+
+    def __str__(self):
+        return self.name
 
 
 class Item(models.Model):
     name = models.CharField(max_length=180)
-    type = models.CharField(max_length=120)  # TODO: ForeignKey
-    category = models.CharField(max_length=120)  # TODO: ForeignKey
+    type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    category = models.ForeignKey(ItemCategory, on_delete=models.CASCADE)
     size = models.CharField(max_length=40)
 
     class Meta:
-        verbose_name = "Item Parent Model"
+        verbose_name = "Item Parent"
         db_table = 'item_parent'
+        abstract = True
+
+
+class Asset(Item):
+    asset_id = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'item_asset'
+
+    def __str__(self):
+        return self.name
